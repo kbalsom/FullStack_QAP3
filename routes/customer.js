0de2723b918ad2f3a6d4adbf5a4b07199240482d
kbalsom <kara.balsom@keyin.com>
2022-11-30 15:16:28 -0330

    Logging successful

--- routes/customer.js
+++ routes/customer.js
@@ -2,19 +2,27 @@ const express = require("express");
 const router = express.Router();
 // const actorsDal = require('../services/pg.actors.dal')
 const custDal = require("../services/m.cust.dal");
+const http = require("http");
+const EventEmitter = require("events"); //Imports the global events module, and assigns it to the constant EventEmiter.
+class MyEmitter extends EventEmitter {} //Creates a class MyEmitter, that inherits the properties of EventEmiiter.
+const myEmitter = new MyEmitter();
+const logger = require("../logger.js");
+
+logger.fileOps();
+
+myEmitter.addListener("status", (msg) => {
+  console.log(msg);
+  logger.addToFile(msg);
+});
 
 router.get("/", async (req, res) => {
-  // const theActors = [
-  //     {first_name: 'Youn', last_name: 'Yuh-jung'},
-  //     {first_name: 'Laura', last_name: 'Dern'},
-  //     {first_name: 'Regina', last_name: 'King'}
-  // ];
   try {
     let theFilms = await custDal.getAllFilms();
     if (DEBUG) console.table(theFilms);
     res.render("customer", { theFilms });
   } catch {
-    res.render("503");
+    res.statusCode(503).res.render("503");
+    myEmitter.emit("status", `Status Code: ${res.statusCode}`);
   }
 });
 
