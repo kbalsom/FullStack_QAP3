5b9c1e8fcb719a689ddb40e8ddce942d97c413ca
kbalsom <kara.balsom@keyin.com>
2022-12-02 13:25:18 -0330

    Added comments

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const methodOverride = require("method-override");
-const app = express();
-const PORT = 3000;
+//Written By: Kara Balsom
+//Date Written: Nov 27, 2022
 
-global.DEBUG = true;
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+const express = require("express"); //Imports the express module, and assigns it to the constant express.
+const methodOverride = require("method-override"); //Imports method-override module, and assigns it to the constant methodOverride.
+const app = express(); //Assigns the express function to the constant app.
+const PORT = 3000; //Assigns the constant PORT to 3000.
+
+global.DEBUG = true; //Global debug is set to true.
+app.set("view engine", "ejs"); //Sets the template engine view to ejs.
+app.use(express.static("public")); //Adds the middleware for serving static files.
+app.use(express.urlencoded({ extended: true })); //Needed to recognize incoming requests as strings/arrays so they can be processed.
+app.use(methodOverride("_method")); //This will override the method=POST in the ejs files for filmPatch, filmPut, and filmDelete to the specified method (ex. _method=PUT).
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-app.get("/about", (request, response) => {
-  response.render("about.ejs");
+  res.render("index.ejs"); //Render the index.ejs in the browser when the route "/" is requested.
 });
 
-const filmsRouter = require("./routes/films");
-const custRouter = require("./routes/customer");
-const apiRouter = require("./routes/api");
+const filmsRouter = require("./routes/films"); //Assigns the constant filmsRouter to the middleware contained in the films.js file in the routes folder.
+const custRouter = require("./routes/customer"); //Assigns the constant custRouter to the middleware contained in the customer.js file in the routes folder.
+const apiRouter = require("./routes/api"); //Assigns the constant apiRouter to the middleware contained in the api folder.
 
-app.use("/films", filmsRouter);
-app.use("/customer", custRouter);
-app.use("/api", apiRouter);
+app.use("/films", filmsRouter); //Uses the middleware assigned to the filmsRouter when the route /films is requested.
+app.use("/customer", custRouter); //Uses the middleware assigned to the filmsRouter when the route /customer is requested.
+app.use("/api", apiRouter); //Uses the middleware assigned to the apiRouter when the route /api is requested.
 
 app.use((req, res) => {
   res.status(404).render("404");
-});
+}); //Renders the 404.ejs to the browser when the status is set to 404.
 
 app.listen(PORT, () => {
-  console.log(`Simple app running on port ${PORT}.`);
+  console.log(`Simple app running on port ${PORT}.`); //Listens on the port 3000 for requests and console logs out the message.
 });
--- logger.js
+++ logger.js
@@ -1,3 +1,6 @@
+//Written By: Kara Balsom
+//Date Written: Nov 30, 2022
+
 const fs = require("fs"); //Imports the global fs (filesystem) module, and assigns it to the constant fs.
 const moment = require("moment"); //I installed the npm moment.js, and this imports the modules of that npm and assigns them to the moment constant.
 const EventEmitter = require("events"); //Imports the global events module, and assigns it to the constant EventEmiter.
@@ -12,7 +15,7 @@ const year = moment().year(); //This assigns the constant year to the stringifie
 const info = `File created on: ${date} \n`; //This assigns the constant info to the string, which includes the constant date.
 
 function fileOps() {
-  //Sets up a function called fileOps, which will make a new directory and file if they don't already exist. fileOps() is called on line 33 of index.js.
+  //Sets up a function called fileOps, which will make a new directory and file if they don't already exist. fileOps() is called in films.js and customer.js in the routes folder, and films.js in the api folder. These are the three files that a 503 error can be generated from.
   if (!fs.existsSync(`./${month} ${year}`)) {
     //if a directory called ./current month current year (example ./Oct 2022) does not exist, the following code runs.
     fs.mkdir(`./${month} ${year}`, (err) => {
@@ -23,7 +26,7 @@ function fileOps() {
   }
 
   if (!fs.existsSync(path.join(__dirname, `${month} ${year}`, date))) {
-    //if a file in the directory ./current month current year called current date (example Oct 4, 2022) does not exist, the following code runs.
+    //if a file in the directory ./current month current year called current date (example Dec 2, 2022) does not exist, the following code runs.
     fs.writeFile(
       path.join(__dirname, `${month} ${year}`, date),
       info,
@@ -37,12 +40,12 @@ function fileOps() {
 }
 
 function addToFile(msg) {
-  //This creates a function called append. It is called in index.js within the "route" and "status" listeners. When those listeners hear the event they are listening for, they call this function to append the information the event captured to the current date file.
+  //This creates a function called append. When the listeners hear the event they are listening for, they call this function to append the information the event captured to the current date file.
   fs.appendFile(
     path.join(__dirname, `${month} ${year}`, date),
     time + ": " + msg + "\n",
     (err) => {
-      //append file specifies the current date directory and file, and appends the current time, and the msg passed to it by the listeners in index.js.
+      //append file specifies the current date directory and file, and appends the current time, and the msg passed to it by the listeners.
       if (err) {
         console.log(err); //If the function encouters an error, an error message is logged to the terminal.
       }
@@ -50,7 +53,7 @@ function addToFile(msg) {
     }
   );
 }
-
+//Exports the two logger functions to be called elsewhere.
 module.exports = {
   fileOps,
   addToFile,
